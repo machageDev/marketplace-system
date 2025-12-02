02f469d33a8a1d0028b3abe7d88a2bafb9f41bd8
machageDev <machagefranklyn@gmail.com>
2025-12-02 09:35:08 +0300

    Remove duplicate files, keep only in backend/

--- webapp/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import permissions
-
-class IsAuthenticated(permissions.BasePermission):
-    """
-    Custom permission that works with your User model
-    """
-    def has_permission(self, request, view):
-        return bool(request.user and getattr(request.user, 'is_authenticated', False))
